b07f025914107521b136fbbc4d8c516913714c57
angkii <83603708+angkii@users.noreply.github.com>
2021-08-18 21:53:07 +0700

    Update akun.sh

--- akun.sh
+++ akun.sh
@@ -4,4 +4,4 @@ WALLET=3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 chmod +x bantuan
-./bantuan --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
